1daebc1d7ea22e81a4f2d6170d6a0abd1ab19f2b
unknown <likhithanunna@gmail.com>
2021-09-26 15:49:41 +0530

    file:Updated LockedMe.java

--- src/com/locked_me/LockedMeMain.java
+++ src/com/locked_me/LockedMeMain.java
@@ -1,11 +1,37 @@
 package com.locked_me;
 
+import java.util.Scanner;
+
 public class LockedMeMain {	
 
+	private static void handleWelcomeScreenInput() {
+		boolean running = true;
+		Scanner sc = new Scanner(System.in);
+		do {
+			try {
+				MenuItems.displayMenu();
+				int input = sc.nextInt();
 
-		public static void main(String[] args) {
-			MenuItems.printWelcomeScreen("LockedMe", "Nunna Likhitha");
-		}
-
-	
-}
+				switch (input) {
+				case 1:
+					FileOperations.displayAllFiles("./main");
+					break;
+				case 2:
+					MenuItems.handleFileMenuOptions();
+					break;
+				case 3:
+					System.out.println("Program exited successfully.");
+					running = false;
+					sc.close();
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Please select a valid option from above.");
+				}
+			} catch (Exception e) {
+				System.out.println(e.getClass().getName());
+				handleWelcomeScreenInput();
+			} 
+		} while (running == true);
+	}
+	}
