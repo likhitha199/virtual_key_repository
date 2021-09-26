7c4a53f01dce329a8ff707ee7d4b1b4d234980a3
unknown <likhithanunna@gmail.com>
2021-09-26 14:33:56 +0530

    file: Added WelcomeScreen.java

---
+++ src/com/locked_me/LockedMeMain.java
@@ -0,0 +1,11 @@
+package com.locked_me;
+
+public class LockedMeMain {	
+
+
+		public static void main(String[] args) {
+			WelcomeScreen.printWelcomeScreen("LockedMe", "Nunna Likhitha");
+		}
+
+	
+}
