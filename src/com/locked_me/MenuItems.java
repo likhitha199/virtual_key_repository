e54b0f34e089867e9bd48e63ebb5b2cd00e4a520
unknown <likhithanunna@gmail.com>
2021-09-28 19:55:14 +0530

    Done

--- src/com/locked_me/MenuItems.java
+++ src/com/locked_me/MenuItems.java
@@ -14,13 +14,13 @@ public class MenuItems {
 		System.out.println(appFunction);
 	}
 	public static void Menu() {
-		String menu = "\n\n****** Select one of the options below and press Enter ******\n\n"
+		String menu = "\n\n****** Select any option number from below and press Enter ******\n\n"
 				+ "1) Retrieve all files inside \"main\" folder\n" + "2) Display menu for File operations\n"
 				+ "3) Exit program\n";
 		System.out.println(menu);
 	}
 	public static void FileMenuOptions() {
-		String fileMenu = "\n\n****** Select one of the options below and press Enter ******\n\n"
+		String fileMenu = "\n\n****** Select any option number from below and press Enter ******\n\n"
 				+ "1) Add a file to \"main\" folder\n" + "2) Delete a file from \"main\" folder\n"
 				+ "3) Search for a file from \"main\" folder\n" + "4) Show Previous Menu\n" + "5) Exit program\n";
 		System.out.println(fileMenu);
